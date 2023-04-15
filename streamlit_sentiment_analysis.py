e7b48d0a086245bc88a8b6b0dba5626d5d0fc8ef
yutan97 <130372303+yutan97@users.noreply.github.com>
2023-04-15 14:53:20 +0700

    Update streamlit_sentiment_analysis.py

--- streamlit_sentiment_analysis.py
+++ streamlit_sentiment_analysis.py
@@ -229,9 +229,9 @@ elif choice == 'Dự đoán':
 			lines = text_process(lines['comment'])
 			x_new = count_model.transform(lines)
 			y_pred_new = sentiment_model.predict(x_new)
-			lines['prediction'] = y_pred_new
+			lines['prediction'] = y_pred_new.tolist()
 			st.write("Kết quả phân tích:")
-			st.dataframe(lines['prediction'])
+			st.dataframe(lines)
 	if type=="Nhập nội dung mới":
 		with st.form(key='my_form'):
 			review = st.text_input(label='Nhập nội dung cần phân tích:')
